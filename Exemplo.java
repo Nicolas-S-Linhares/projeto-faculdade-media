3797efd60e3b6e0c020733b9ad9e4671e0b96c42
Nicolas Sezerino Linhares <nicolassezerinolinhares@gmail.com>
2025-02-24 10:31:53 -0300

    media

---
+++ Exemplo.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Exemplo { 
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Nota 1:");
+        float n1 = teclado.nextFloat();
+
+        System.out.println("Nota 2:");
+        float n2 = teclado.nextFloat();
+
+        System.out.println("Nota 3:");
+        float n3 = teclado.nextFloat();
+
+        System.out.println("Nome:");
+        String nome = teclado.nextLine();
+
+        float media = (n1 + n2 + n3) / 3;
+
+        System.out.println(nome);
+        System.out.println(media);
+        
+    }
+}
