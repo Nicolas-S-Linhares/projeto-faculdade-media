7062fb1a134847d4d06b7fff700366f594455861
Nicolas Sezerino Linhares <nicolassezerinolinhares@gmail.com>
2025-02-24 10:50:34 -0300

    exemplos das atividades

---
+++ exemplo2.java
@@ -0,0 +1,15 @@
+public class exemplo2 {
+
+    //Leia 2 valores inteiros e
+    //imprima sua multiplicação.
+
+    //Entrada: valor1, valor2
+    //Saida: multiplicação
+    //Processo: multiplicar = valor1 * valor 2
+    //Teste
+    //Valor1  |
+    //Valor2
+    public static void main(String[] args) {
+        
+    }
+}
