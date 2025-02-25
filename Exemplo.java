664a9ce995ad0054dc71044112dcc4ec5e01351e
Nicolas Sezerino Linhares <seuemail@nicolassezerinolinhares@gmail.com>
2025-02-25 08:21:02 -0300

    mudancas no readme

--- Exemplo.java
+++ Exemplo.java
@@ -16,6 +16,7 @@ public class Exemplo {
         float n3 = teclado.nextFloat();
 
         System.out.println("Nome:");
+        teclado.nextLine();
         String nome = teclado.nextLine();
 
         float media = (n1 + n2 + n3) / 3;
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-##Media aluno
+## Media aluno
 
 ![image](https://github.com/user-attachments/assets/e7435069-2101-45cc-9260-4753727359cd)
