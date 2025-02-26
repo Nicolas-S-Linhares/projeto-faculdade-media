dc66665334a9781f9545dd795f7347c12ed12119
Nicolas Sezerino Linhares <129209443+Nicolas-S-Linhares@users.noreply.github.com>
2025-02-26 12:40:04 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,83 +1,47 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class JogoDaVelha {
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        JogoDaVelha_Mapa mapa = new JogoDaVelha_Mapa();
-        JogoDaVelha_Jogador jogador = new JogoDaVelha_Jogador(teclado);
-        JogoDaVelha_PC pc = new JogoDaVelha_PC();
-        
-        boolean jogarNovamente;
-        do {
-            mapa.limparMapa();
-            boolean vezDoJogador = mapa.sortearInicio();
-            int jogadas = 0;
-            boolean jogoEmAndamento = true;
-            
-            while (jogoEmAndamento) {
-                mapa.desenharMapa(jogadas);
-                
-                if (vezDoJogador) {
-                    jogador.jogar(mapa);
-                    if (mapa.verificarVitoria('X')) {
-                        System.out.println("... Jogador GANHOU!");
-                        jogoEmAndamento = false;
-                    }
-                } else {
-                    pc.jogar(mapa);
-                    if (mapa.verificarVitoria('O')) {
-                        System.out.println("... PC GANHOU!");
-                        jogoEmAndamento = false;
-                    }
-                }
-                
-                jogadas++;
-                if (jogoEmAndamento && jogadas == 9) {
-                    mapa.desenharMapa(jogadas);
-                    System.out.println("... EMPATOU!");
-                    jogoEmAndamento = false;
-                }
-                
-                vezDoJogador = !vezDoJogador;
-            }
-            
-            System.out.print("Deseja jogar novamente (s/n)? ");
-            jogarNovamente = teclado.next().equalsIgnoreCase("s");
-            
-        } while (jogarNovamente);
-        teclado.close();
-    }
-}
-
 class JogoDaVelha_Mapa {
-    private char[][] mapa = new char[3][3];
-    private Random random = new Random();
-    
-    public void limparMapa() {
+    private char[][] mapa;
+
+    public JogoDaVelha_Mapa() {
+        mapa = new char[3][3];
+        limpaMapa();
+    }
+
+    public int sortear(int inicio, int fim) {
+        Random random = new Random();
+        return random.nextInt(fim - inicio + 1) + inicio;
+    }
+
+    public void limpaMapa() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 mapa[i][j] = ' ';
             }
         }
     }
-    
-    public boolean sortearInicio() {
-        return random.nextBoolean();
-    }
-    
-    public void desenharMapa(int jogada) {
-        System.out.println("------------- .. jogada: " + jogada);
+
+    public void desenhaMapa() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(" " + mapa[i][j] + " ");
-                if (j < 2) System.out.print("|");
+                System.out.print(mapa[i][j]);
+                if (j < 2) System.out.print(" | ");
             }
-            System.out.println("\n-------------");
+            System.out.println();
+            if (i < 2) System.out.println("---------");
         }
     }
-    
-    public boolean verificarVitoria(char jogador) {
+
+    public boolean jogar(int l, int c, char jogador) {
+        if (l >= 0 && l < 3 && c >= 0 && c < 3 && mapa[l][c] == ' ') {
+            mapa[l][c] = jogador;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean ganhou(char jogador) {
         for (int i = 0; i < 3; i++) {
             if (mapa[i][0] == jogador && mapa[i][1] == jogador && mapa[i][2] == jogador) return true;
             if (mapa[0][i] == jogador && mapa[1][i] == jogador && mapa[2][i] == jogador) return true;
@@ -86,48 +50,88 @@ class JogoDaVelha_Mapa {
         if (mapa[0][2] == jogador && mapa[1][1] == jogador && mapa[2][0] == jogador) return true;
         return false;
     }
-    
-    public boolean marcarJogada(int linha, int coluna, char jogador) {
-        if (linha >= 0 && linha < 3 && coluna >= 0 && coluna < 3 && mapa[linha][coluna] == ' ') {
-            mapa[linha][coluna] = jogador;
-            return true;
-        }
-        return false;
-    }
 }
 
 class JogoDaVelha_Jogador {
-    private Scanner teclado;
-    
-    public JogoDaVelha_Jogador(Scanner teclado) {
-        this.teclado = teclado;
+    private JogoDaVelha_Mapa mapa;
+    private char letra;
+
+    public JogoDaVelha_Jogador(JogoDaVelha_Mapa mapa) {
+        this.mapa = mapa;
+        this.letra = 'X';
     }
-    
-    public void jogar(JogoDaVelha_Mapa mapa) {
-        int linha, coluna;
-        boolean jogadaValida;
-        do {
-            System.out.println("Jogador ..\n  linha: ");
-            linha = teclado.nextInt() - 1;
-            System.out.println("  coluna: ");
-            coluna = teclado.nextInt() - 1;
-            jogadaValida = mapa.marcarJogada(linha, coluna, 'X');
-            if (!jogadaValida) System.out.println("Posição inválida! Tente novamente.");
-        } while (!jogadaValida);
+
+    public boolean joga(Scanner teclado) {
+        System.out.println("Jogador " + letra + ", escolha a linha e a coluna (1-3 para ambos):");
+        System.out.print("Linha: ");
+        int linha = teclado.nextInt() - 1;
+        System.out.print("Coluna: ");
+        int coluna = teclado.nextInt() - 1;
+
+        return mapa.jogar(linha, coluna, letra);
     }
 }
 
 class JogoDaVelha_PC {
-    private Random random = new Random();
-    
-    public void jogar(JogoDaVelha_Mapa mapa) {
+    private JogoDaVelha_Mapa mapa;
+    private char letra = 'O';
+
+    public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
+        this.mapa = mapa;
+    }
+
+    public boolean joga() {
+        Random random = new Random();
         int linha, coluna;
-        boolean jogadaValida;
         do {
             linha = random.nextInt(3);
             coluna = random.nextInt(3);
-            jogadaValida = mapa.marcarJogada(linha, coluna, 'O');
-        } while (!jogadaValida);
-        System.out.println("PC[" + linha + "," + coluna + "]");
+        } while (!mapa.jogar(linha, coluna, letra));
+        return true;
+    }
+}
+
+public class JogoDaVelha {
+    private JogoDaVelha_Mapa jogoMapa;
+    private JogoDaVelha_PC pc;
+    private JogoDaVelha_Jogador jogador;
+
+    public JogoDaVelha() {
+        jogoMapa = new JogoDaVelha_Mapa();
+        jogador = new JogoDaVelha_Jogador(jogoMapa);
+        pc = new JogoDaVelha_PC(jogoMapa);
+    }
+
+    public void jogar(Scanner teclado) {
+        jogoMapa.limpaMapa();
+        char jogadorAtual = (jogoMapa.sortear(0, 1) == 0) ? 'X' : 'O';
+        boolean jogoEmAndamento = true;
+
+        while (jogoEmAndamento) {
+            jogoMapa.desenhaMapa();
+            boolean jogadaValida;
+            if (jogadorAtual == 'X') {
+                jogadaValida = jogador.joga(teclado);
+            } else {
+                jogadaValida = pc.joga();
+            }
+
+            if (jogadaValida) {
+                if (jogoMapa.ganhou(jogadorAtual)) {
+                    jogoMapa.desenhaMapa();
+                    System.out.println("Jogador " + jogadorAtual + " venceu!");
+                    jogoEmAndamento = false;
+                } else {
+                    jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        JogoDaVelha jogo = new JogoDaVelha();
+        jogo.jogar(teclado);
+        teclado.close();
     }
 }
